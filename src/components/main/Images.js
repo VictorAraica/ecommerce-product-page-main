8401817ce16330fde52456e682c1e4ae274eeba1
Víctor Araica <victoraraica@hotmail.com>
2022-01-22 18:20:52 -0400

    fixed alt in image

--- src/components/main/Images.js
+++ src/components/main/Images.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { ImagesContainerStyled, ImageStyled, RelativeDiv } from "./styles/ImagesStyled";
+import {
+  ImagesContainerStyled,
+  ImageStyled,
+  RelativeDiv,
+} from "./styles/ImagesStyled";
 import Thumbnails from "./Thumbnails";
 import ImagesButton from "./ImagesButton";
 
@@ -8,17 +12,27 @@ export default function Images({ images, index, setIndex, setModal }) {
     <ImagesContainerStyled>
       <RelativeDiv>
         {index > 0 ? (
-          <ImagesButton right={false} index={index} setIndex={setIndex} inModal={false}/>
+          <ImagesButton
+            right={false}
+            index={index}
+            setIndex={setIndex}
+            inModal={false}
+          />
         ) : (
           ""
         )}
         <ImageStyled
           src={images[index].image}
-          alt="hola"
+          alt="main image"
           onClick={() => (window.innerWidth > 890 ? setModal(true) : "")}
         />
         {index < images.length - 1 ? (
-          <ImagesButton right={true} index={index} setIndex={setIndex} inModal={false}/>
+          <ImagesButton
+            right={true}
+            index={index}
+            setIndex={setIndex}
+            inModal={false}
+          />
         ) : (
           ""
         )}
